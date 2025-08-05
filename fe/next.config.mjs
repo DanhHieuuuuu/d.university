23c76d8d4929b1782de849a5bb505f9c57b7dcd0
DucThanh <phoducthanh@gmail.com>
2025-08-06 02:57:11 +0700

    add base fe

---
+++ fe/next.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true, // Recommended for the `pages` directory, default in `app`.
+  sassOptions: {},
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+        port: '',
+        pathname: '**'
+      }
+    ]
+  },
+  env: {}};
+
+export default nextConfig;
