57f80b0f8b8de8fa564b88c425bbb2df7c3aaaf2
DucThanh <phoducthanh@gmail.com>
2025-09-10 18:29:16 +0700

    add tbl NhanSu to db

--- be/Library/D.ControllerBase/ProgramBase.cs
+++ be/Library/D.ControllerBase/ProgramBase.cs
@@ -104,7 +104,7 @@ namespace D.ControllerBases
                 options =>
                 {
                     options.UseSqlServer(
-                        builder.Configuration.GetConnectionString("Default"),
+                        builder.Configuration.GetConnectionString("Core"),
 
                         options =>
                         {
---
+++ be/Services/Core/D.Core.Domain/Migrations/20250910112559_init.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace D.Core.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "hrm");
+
+            migrationBuilder.CreateTable(
+                name: "NsNhanSu",
+                schema: "hrm",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MaNhanSu = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    HoDem = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Ten = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NgaySinh = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    NoiSinh = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    GioiTinh = table.Column<bool>(type: "bit", nullable: true),
+                    QuocTich = table.Column<int>(type: "int", nullable: true),
+                    DanToc = table.Column<int>(type: "int", nullable: true),
+                    TonGiao = table.Column<int>(type: "int", nullable: true),
+                    NguyenQuan = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NoiOHienTai = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SoCccd = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NgayCapCccd = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    NoiCapCccd = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SoDienThoai = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    KhanCapSoDienThoai = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    KhanCapNguoiLienHe = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    MaSoThue = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TenNganHang1 = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Atm1 = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TenNganHang2 = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Atm2 = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    HienTaiChucVu = table.Column<int>(type: "int", nullable: true),
+                    HienTaiPhongBan = table.Column<int>(type: "int", nullable: true),
+                    IdLoaiHopDong = table.Column<int>(type: "int", nullable: true),
+                    IdHopDong = table.Column<int>(type: "int", nullable: true),
+                    DaChamDutHopDong = table.Column<bool>(type: "bit", nullable: true),
+                    DaVeHuu = table.Column<bool>(type: "bit", nullable: true),
+                    IsThoiViec = table.Column<bool>(type: "bit", nullable: true),
+                    CreatedBy = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedBy = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: true),
+                    ModifiedDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Deleted = table.Column<bool>(type: "bit", nullable: false),
+                    DeletedBy = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: true),
+                    DeletedDate = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NsNhanSu", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "NsNhanSu",
+                schema: "hrm");
+        }
+    }
+}
