2324948a4c0ea0312ae52b835ff684a7aa371b68
DanhHieuuuuu <hieunguyendanh91@gmail.com>
2025-09-19 10:15:10 +0700

    permission key constant

---
+++ be/Services/Shared/d.Shared.Permission/PermissionKeyConstant.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d.Shared.Permission
+{
+    public static class PermissionKeyConstant
+    {
+        public const string Admin = "admin";
+    }
+}
