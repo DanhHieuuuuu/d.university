b6d567ae500082c0a0ef3dead227bcedc3df8619
DanhHieuuuuu <hieunguyendanh91@gmail.com>
2025-09-29 19:32:21 +0700

    add func email

---
+++ be/Library/D.InfrastructureBase/Shared/SendNotification.cs
@@ -0,0 +1,31 @@
+﻿using D.DomainBase.Dto;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D.InfrastructureBase.Shared
+{
+    public static class SendNotification
+    {
+        public static async Task SendEmail(SendEmailDto dto)
+        {
+            var smtpClient = new SmtpClient(dto.Host, dto.Post);
+
+            smtpClient.EnableSsl = true;
+            smtpClient.UseDefaultCredentials = false;
+            smtpClient.Credentials = new NetworkCredential(dto.EmailFrom, dto.Password);
+
+            var message = new MailMessage(dto.EmailFrom, dto.EmailTo, dto.Title, dto.Body)
+            {
+                IsBodyHtml = true
+            };
+
+            await smtpClient.SendMailAsync(message);
+        }
+    }
+}
