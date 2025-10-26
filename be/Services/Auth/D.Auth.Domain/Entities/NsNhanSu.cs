288e0e7b42839f9ff19cfcdd091c9399be7b3931
PhamDangKhoa134 <phamdangkhoa134@gmail.com>
2025-10-26 09:34:46 +0700

    update trường ImageLink

--- be/Services/Auth/D.Auth.Domain/Entities/NsNhanSu.cs
+++ be/Services/Auth/D.Auth.Domain/Entities/NsNhanSu.cs
@@ -68,4 +68,5 @@ public partial class NsNhanSu : EntityBase
     public string? Password { get; set; }
 
     public string? PasswordKey { get; set; }
+    public string? ImageLink { get; set; }
 }
---
+++ be/Services/Auth/D.Auth.Domain/Migrations/20251026010730_InitDatabase.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace D.Auth.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageLink",
+                schema: "hrm",
+                table: "NsNhanSu",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageLink",
+                schema: "hrm",
+                table: "NsNhanSu");
+        }
+    }
+}
