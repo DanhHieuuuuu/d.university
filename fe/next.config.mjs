3a865856c18f5178d682234216eb5d59c08669e9
DanhHieuuuuu <hieunguyendanh91@gmail.com>
2026-01-04 10:21:15 +0700

    disable eslint

--- fe/next.config.mjs
+++ fe/next.config.mjs
@@ -8,11 +8,16 @@ const nextConfig = {
         protocol: 'https',
         hostname: '**',
         port: '',
-        pathname: '**'
-      }
-    ]
+        pathname: '**',
+      },
+    ],
   },
-  env: {}
+  env: {},
+
+  // 🔥 Disable ESLint when build (Vercel sẽ không fail vì ESLint nữa)
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
