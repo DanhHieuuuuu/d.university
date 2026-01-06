f5d8589895737de09abd80fdf3a16be46c724a22
BaThanh <115627461+Tlordisme@users.noreply.github.com>
2026-01-06 17:41:56 +0700

    update Table

---
+++ be/Services/Auth/D.Auth.Domain/Migrations/20260106101804_Init_Notification.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace D.Auth.Domain.Migrations.NotificationDb
+{
+    /// <inheritdoc />
+    public partial class Init_Notification : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
