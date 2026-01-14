cdf112022bb751c96b61c40a71ec1238cad61074
DanhHieuuuuu <hieunguyendanh91@gmail.com>
2026-01-14 23:57:08 +0700

    service supporter

--- be/Services/Core/D.Core.Domain/Dtos/Delegation/Incoming/DelegationIncoming/CreateSupporterRequestDto.cs
+++ be/Services/Core/D.Core.Domain/Dtos/Delegation/Incoming/DelegationIncoming/CreateSupporterRequestDto.cs
@@ -1,23 +1,24 @@
-﻿using D.DomainBase.Common;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using D.DomainBase.Common; 
+using System; 
+using System.Collections.Generic; 
+using System.ComponentModel.DataAnnotations; 
+using System.Linq; 
+using System.Text; 
+using System.Threading.Tasks; 
+ 
 namespace D.Core.Domain.Dtos.Delegation.Incoming.DelegationIncoming
 {
-    public class CreateSupporterRequestDto : ICommand<List<CreateSupporterResponseDto>>
-    {
-        public int DepartmentSupportId { get; set; }
-
-        public List<SupporterItemDto> Supporters { get; set; } = new();
-    }
-
-    public class SupporterItemDto
-    {
-        public int SupporterId { get; set; }
-        public string? SupporterCode { get; set; }
-    }
-}
+    public class CreateSupporterRequestDto : ICommand<List<CreateSupporterResponseDto>> 
+    { 
+        public int DepartmentSupportId { get; set; } 
+         
+        public List<SupporterItemDto> Supporters { get; set; } = new();  
+    } 
+     
+    public class SupporterItemDto 
+    { 
+        public int SupporterId { get; set; } 
+        public string? SupporterCode { get; set; } 
+    } 
+} 
+   
