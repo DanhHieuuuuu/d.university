0a7c5a785cfbc94e1f4b8b147be77448d3d1564a
DanhHieuuuuu <hieunguyendanh91@gmail.com>
2026-01-24 23:03:07 +0700

    Gán notifihub

--- be/Library/D.Notification/Infrastructures/Hubs/NotificationHub.cs
+++ be/Library/D.Notification/Infrastructures/Hubs/NotificationHub.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
 
 namespace D.Notification.Infrastructure.Hubs
 {
+    [Authorize]
     public class NotificationHub : Hub
     {
         public override async Task OnConnectedAsync()
