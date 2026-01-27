ee61507fea13c1b0159f5da533b84d47ec3050e4
ndeeuish <120817902+ndeeuish@users.noreply.github.com>
2026-01-27 21:34:37 +0700

    fix submission

---
+++ be/Services/Core/D.Core.Domain/Migrations/20260127140227_updatesurvey5.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace D.Core.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatesurvey5 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserType",
+                schema: "ks",
+                table: "KsSurveySubmission",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            
+
+            migrationBuilder.DropColumn(
+                name: "UserType",
+                schema: "ks",
+                table: "KsSurveySubmission");
+
+            
+        }
+    }
+}
