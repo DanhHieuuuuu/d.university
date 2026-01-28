f992afb8775385ef0d7d4e2615f0540940c59170
DanhHieuuuuu <hieunguyendanh91@gmail.com>
2026-01-28 12:44:10 +0700

    Chuyển hết url vào env

--- fe/app/manager/file/page.tsx
+++ fe/app/manager/file/page.tsx
@@ -47,8 +47,7 @@ const Page = () => {
   const [previewImage, setPreviewImage] = useState<string>('');
 
   const getImageUrl = (fileName: string) => {
-    const baseUrl = process.env.NEXT_PUBLIC_API_URL || 'https://d-university-9zz7.onrender.com/api';
-    return `${baseUrl}/s3-test/download?fileName=${encodeURIComponent(fileName)}`;
+    return `${process.env.NEXT_PUBLIC_AUTH_API_URL}s3-test/download?fileName=${encodeURIComponent(fileName)}`;
   };
 
   const { query, pagination, onFilterChange, resetFilter } = usePaginationWithFilter({
