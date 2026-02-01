2ae4d7c359e1c391f0b49afd7dc61168f3ae7ad2
DucThanh <phoducthanh@gmail.com>
2026-02-02 00:58:57 +0700

    feat(danh-muc): add func delete danh-muc

--- be/Services/Core/D.Core.Domain/Dtos/Hrm/DanhMuc/DmToBoMon/DmToBoMonRequestDto.cs
+++ be/Services/Core/D.Core.Domain/Dtos/Hrm/DanhMuc/DmToBoMon/DmToBoMonRequestDto.cs
@@ -1,10 +1,12 @@
 ﻿using D.DomainBase.Common;
 using D.DomainBase.Dto;
+using Microsoft.AspNetCore.Mvc;
 
 namespace D.Core.Domain.Dtos.Hrm.DanhMuc.DmToBoMon
 {
     public class DmToBoMonRequestDto : FilterBaseDto, IQuery<PageResultDto<DmToBoMonResponseDto>>
     {
-
+        [FromQuery(Name = "idPhongBan")]
+        public int? IdPhongBan { get; set; }
     }
 }
